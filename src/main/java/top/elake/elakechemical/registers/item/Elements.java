906e27123a6e0f9187800b7164bebf6e7dcb9881
Erhai-lake <fuzixuan0714_0826@163.com>
2024-10-17 20:10:11 +0800

    .

--- src/main/java/top/elake/elakechemical/registers/item/Elements.java
+++ src/main/java/top/elake/elakechemical/registers/item/Elements.java
@@ -1,18 +1,21 @@
 package top.elake.elakechemical.registers.item;
 
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Rarity;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredItem;
-import top.elake.elakechemical.ElakeChemical;
 import net.neoforged.neoforge.registries.DeferredRegister;
-import net.minecraft.world.item.Item;
+import top.elake.elakechemical.ElakeChemical;
 
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * @author Erhai_lake
+ */
 public class Elements {
     public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(ElakeChemical.MODID);
-    public static final List<DeferredItem<Item>> REGISTERED_ELEMENTS = new ArrayList<DeferredItem<Item>>();
+    public static final List<DeferredItem<Item>> REGISTERED_ELEMENTS = new ArrayList<>();
 
     public static String[] ElementsItemArr = new String[]{
             "hydrogen", "helium", "lithium", "beryllium", "boron", "carbon", "nitrogen", "oxygen", "fluorine", "neon", "natrium", "magnesium", "aluminium", "silicon", "phosphorus", "sulfur", "chlorine", "argon", "kalium", "calcium", "scandium", "titanium", "vanadium", "chromium", "manganese", "ferrum", "cobalt", "nickel", "cuprum", "zinc", "gallium", "germanium", "arsenic", "selenium", "bromine", "krypton", "rubidium", "strontium", "yttrium", "zirconium", "niobium", "molybdenum", "technetium", "ruthenium", "rhodium", "palladium", "argentum", "cadmium", "indium", "stannum", "stibium", "tellurium", "iodine", "xenon", "cesium", "barium", "lanthanum", "cerium", "praseodymium", "neodymium", "promethium", "samarium", "europium", "gadolinium", "terbium", "dysprosium", "holmium", "erbium", "thulium", "ytterbium", "lutetium", "hafnium", "tantalum", "tungsten", "rhenium", "osmium", "iridium", "platinum", "aurum", "hydrargyrum", "thallium", "lead", "bismuth", "polonium", "astatine", "radon", "francium", "radium", "actinium", "thorium", "protactinium", "uranium", "neptunium", "plutonium", "americium", "curium", "berkelium", "californium", "einsteinium", "fermium", "mendelevium", "nobelium", "lawrencium", "rutherfordium", "dubnium", "seaborgium", "bohrium", "hassium", "meitnerium", "darmstadtium", "roentgenium", "copernicium", "nihonium", "flerovium", "moscovium", "livermorium", "tennessine", "oganesson"
