38bdeb9e1952e0c17e2c00282fadff897c31bfed
Erhai-lake <fuzixuan0714_0826@163.com>
2024-10-17 16:09:51 +0800

    新增: 画错误的BUG

--- src/main/java/top/elake/registers/Paintings.java
+++ src/main/java/top/elake/registers/Paintings.java
@@ -18,7 +18,7 @@ public class Paintings {
     public static final DeferredRegister<PaintingVariant> PAINTING_VARIANTS = DeferredRegister.create(Registries.PAINTING_VARIANT, ElakeChemical.MODID);
 
     public static final Supplier<PaintingVariant> PERIODIC_TABLE_OF_CHEMICAL_ELEMENTS = PAINTING_VARIANTS.register("periodic_table_of_chemical_elements", () -> {
-        ResourceLocation texture = requireNonNull(ResourceLocation.parse("elake_chemical:textures/painting/periodic_table_of_chemical_elements.png"));
+        ResourceLocation texture = requireNonNull(ResourceLocation.tryBuild(ElakeChemical.MODID, "textures/painting/periodic_table_of_chemical_elements.png"));
         return new PaintingVariant(64 * 2, 64, texture);
     });
 
