400b5248ecd61d3abbc7d2811e6522e23c0db8a4
Erhai-lake <fuzixuan0714_0826@163.com>
2024-10-25 02:02:29 +0800

    Test ItemColor

--- src/main/java/top/elake/elakechemical/client/color/ItemColor.java
+++ src/main/java/top/elake/elakechemical/client/color/ItemColor.java
@@ -1,14 +1,20 @@
 package top.elake.elakechemical.client.color;
 
-import net.minecraft.client.color.item.ItemColors;
-import net.minecraft.world.item.Items;
+import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.client.event.RegisterColorHandlersEvent;
+import top.elake.elakechemical.ElakeChemical;
 import top.elake.elakechemical.registers.item.Sundries;
 
+/**
+ * @author Qi-Month
+ */
+@EventBusSubscriber(modid = ElakeChemical.MODID, bus = EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
 public class ItemColor {
     @SubscribeEvent
     public static void registerItemColors(RegisterColorHandlersEvent.Item event) {
-        event.register(Sundries.TEST_INGOT);
+        event.register((stack, tintIndex) -> 0xff0000ff, Sundries.TEST_INGOT
+        );
     }
 }
--- src/main/java/top/elake/elakechemical/registers/item/Sundries.java
+++ src/main/java/top/elake/elakechemical/registers/item/Sundries.java
@@ -2,8 +2,8 @@ package top.elake.elakechemical.registers.item;
 
 import net.minecraft.world.item.Item;
 import net.neoforged.neoforge.registries.DeferredItem;
-import top.elake.elakechemical.utils.Registers;
 import top.elake.elakechemical.registers.ModCreativeModeTab;
+import top.elake.elakechemical.utils.Registers;
 
 /**
  * @author Erhai-lake
@@ -15,6 +15,7 @@ public class Sundries {
     public static void register() {
         ModCreativeModeTab.addSubItems(Registers.registerItem("grass_fiber", new Item.Properties()));
         ModCreativeModeTab.addSubItems(Registers.registerItem("grass_string", new Item.Properties()));
+        ModCreativeModeTab.addSubItems(TEST_INGOT);
     }
 
     public static final DeferredItem<Item> TEST_INGOT = Registers.registerItem("test_ingot", new Item.Properties());
