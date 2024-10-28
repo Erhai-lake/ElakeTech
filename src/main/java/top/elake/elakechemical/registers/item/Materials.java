bb1da062a00f6fe7f2be2cd811fbd2a3494b52ba
Qi-Month <dy757410@163.com>
2024-10-28 18:54:38 +0800

    新增已知新增 移除已知移除 修改已知修改 修复已知修复 优化已知优化 补全已知补全 推进已知推进 提交已知提交 推送已知推送 配置已知配置 保留已知保留 调试已知调试 神金已知神金 发颠已知发颠 牢骚已知牢骚 骂街已知骂街 吐槽已知吐槽

--- src/main/java/top/elake/elakechemical/registers/item/Materials.java
+++ src/main/java/top/elake/elakechemical/registers/item/Materials.java
@@ -5,10 +5,29 @@ import net.neoforged.neoforge.registries.DeferredItem;
 import top.elake.elakechemical.registers.ModCreativeModeTab;
 import top.elake.elakechemical.utils.Registers;
 
+/**
+ * @author Qi-Month
+ */
 public class Materials {
+    /**
+     * 注册
+     */
     public static void register() {
         ModCreativeModeTab.addSubItems(COPPER_PLATE);
+        ModCreativeModeTab.addSubItems(COPPER_WIRE);
+        ModCreativeModeTab.addSubItems(COPPER_SPOOL);
     }
 
+    /**
+     * 铜板
+     */
     public static final DeferredItem<Item> COPPER_PLATE = Registers.registerItem("copper_plate", new Item.Properties());
+    /**
+     * 铜线
+     */
+    public static final DeferredItem<Item> COPPER_WIRE = Registers.registerItem("copper_wire", new Item.Properties());
+    /**
+     * 铜单轴线圈
+     */
+    public static final DeferredItem<Item> COPPER_SPOOL = Registers.registerItem("copper_spool", new Item.Properties());
 }
