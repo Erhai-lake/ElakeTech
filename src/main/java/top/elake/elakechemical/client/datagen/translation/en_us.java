66b1e1e31a6a43ec4496c9f604400c8c26cd9345
Erhai-lake <fuzixuan0714_0826@163.com>
2024-10-29 23:34:39 +0800

    修改: i18n全部完成!

--- src/main/java/top/elake/elakechemical/client/datagen/translation/en_us.java
+++ src/main/java/top/elake/elakechemical/client/datagen/translation/en_us.java
@@ -17,41 +17,6 @@ public class en_us extends LanguageProvider {
 
     @Override
     protected void addTranslations() {
-        // 标签栏
-        // 全部
-//        add("itemGroup.elake_chemical.all", "ElakeChemical - All");
-
-        // 元素
-//        for (DeferredItem<? extends Item> element : ModCreativeModeTab.REGISTERED_ELEMENTS) {
-//            add(element.get(), element.getRegisteredName().replace(ElakeChemical.MODID + ":", ""));
-//        }
-        // 材料
-        // 草纤维
-//        add(Materials.GRASS_FIBER.get(), "Grass Fiber");
-        // 草绳
-//        add(Materials.GRASS_STRING.get(), "Grass String");
-        // 铜板
-//        add(Materials.COPPER_PLATE.get(), "Copper Plate");
-        // 铜线
-//        add(Materials.COPPER_WIRE.get(), "Copper Wire");
-        // 铜简易单闸线圈
-//        add(Materials.SIMPLE_COPPER_SINGLE_SOLENOID.get(), "Simple Copper Single Solenoid");
-        // 石墨
-//        add(Materials.GRAPHITE.get(), "Graphite");
-        // 燧石剑
-//        add(Flint.FLINT_SWORD.get(), "Flint Sword");
-        // 燧石镐
-//        add(Flint.FLINT_PICKAXE.get(), "Flint Pickaxe");
-        // 燧石斧
-//        add(Flint.FLINT_AXE.get(), "Flint Axe");
-        // 燧石手斧
-//        add(Flint.FLINT_HANDAXE.get(), "Flint Handaxe");
-        // 燧石锹
-//        add(Flint.FLINT_SHOVEL.get(), "Flint Shovel");
-        // 燧石锄
-//        add(Flint.FLINT_HOE.get(), "Flint Hoe");
-        // 石锤
-//        add(Hammer.STONE_HAMMER.get(), "Stone Hammer");
         for (List<String> item : I18n.LIST) {
             add(item.get(0), item.get(1));
         }
