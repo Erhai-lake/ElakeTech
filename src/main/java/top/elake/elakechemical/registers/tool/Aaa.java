2b6940c432d875ff6873c4a5204e6093faabaaf3
Erhai-lake <fuzixuan0714_0826@163.com>
2024-10-30 18:56:27 +0800

    新增: 无法运行的BUG

--- src/main/java/top/elake/elakechemical/registers/tool/Aaa.java
+++ src/main/java/top/elake/elakechemical/registers/tool/Aaa.java
@@ -1,21 +1,25 @@
 package top.elake.elakechemical.registers.tool;
 
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.item.PickaxeItem;
-import net.minecraft.world.item.Tiers;
+import net.minecraft.world.item.Tier;
 import org.jetbrains.annotations.NotNull;
 
 /**
  * @author Erhai-lake
  */
 public class Aaa extends PickaxeItem {
-    public Aaa() {
-        super(Tiers.STONE, new Properties());
+    public Aaa(Tier tier, Properties properties) {
+        super(tier, properties);
     }
 
     @Override
-    public @NotNull ItemStack getCraftingRemainingItem(@NotNull ItemStack stack) {
+    public @NotNull ItemStack getCraftingRemainingItem(ItemStack stack) {
         int damageValue = stack.getDamageValue();
+        if (damageValue >= 1) {
+            return new ItemStack(Items.AIR);
+        }
         stack.setDamageValue(damageValue + 1);
         return stack;
     }
--- src/main/java/top/elake/elakechemical/registers/tool/Hammer.java
+++ src/main/java/top/elake/elakechemical/registers/tool/Hammer.java
@@ -1,11 +1,13 @@
 package top.elake.elakechemical.registers.tool;
 
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.PickaxeItem;
 import net.minecraft.world.item.Tiers;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredItem;
 import net.neoforged.neoforge.registries.DeferredRegister;
+import org.jetbrains.annotations.NotNull;
 import top.elake.elakechemical.ElakeChemical;
 import top.elake.elakechemical.registers.ModCreativeModeTab;
 
@@ -22,24 +24,19 @@ public class Hammer {
 //            .attributes(HoeItem.createAttributes(Tiers.STONE, 1, -2))
 //    );
 
-    public static final DeferredItem<Item> STONE_HAMMER = ITEMS.register("stone_hammer", () -> new PickaxeItem(Tiers.STONE, new Item.Properties()
-            .attributes(PickaxeItem.createAttributes(Tiers.STONE, 1, -2)))
-            .getCraftingRemainingItem()
-    );
+//    public static final DeferredItem<Item> STONE_HAMMER = ITEMS.register("stone_hammer", () -> new Aaa(Tiers.STONE, new Item.Properties()
+//            .attributes(PickaxeItem.createAttributes(Tiers.STONE, 1, -2)))
+//            .getCraftingRemainingItem()
+//    );
 
-//    public static final DeferredItem<PickaxeItem> STONE_HAMMER = ITEMS.register("stone_hammer", () -> {
-//        return new PickaxeItem(Tiers.STONE, new Item.Properties()
-//                .stacksTo(1)
-//                .durability(Tiers.STONE.getUses())) {
-//            @Override
-//            public @NotNull ItemStack getCraftingRemainingItem(@NotNull ItemStack itemStack) {
-                //                if (remainingStack.getDamageValue() < remainingStack.getMaxDamage() - 1) {
-//                    remainingStack.setDamageValue(remainingStack.getDamageValue() + 1);
-//                }
-//                return super.getCraftingRemainingItem(itemStack);
-//            }
-//        };
-//    });
+        public static final DeferredItem<PickaxeItem> STONE_HAMMER = ITEMS.register("stone_hammer", () -> new Aaa(Tiers.STONE, new Item.Properties()
+                .stacksTo(1)
+                .durability(Tiers.STONE.getUses())) {
+            @Override
+            public @NotNull ItemStack getCraftingRemainingItem(@NotNull ItemStack itemStack) {
+                return super.getCraftingRemainingItem(itemStack);
+            }
+        });
 
 
     /**
