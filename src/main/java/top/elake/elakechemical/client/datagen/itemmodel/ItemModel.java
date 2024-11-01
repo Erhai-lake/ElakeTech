cbcab809e817ecaed5c5a2643fa8969f86a332d6
Erhai-lake <fuzixuan0714_0826@163.com>
2024-11-01 15:32:59 +0800

    修改: 使用物品模型数据生成器

--- src/main/java/top/elake/elakechemical/client/datagen/itemmodel/ItemModel.java
+++ src/main/java/top/elake/elakechemical/client/datagen/itemmodel/ItemModel.java
@@ -1,5 +1,7 @@
 package top.elake.elakechemical.client.datagen.itemmodel;
 
+import top.elake.elakechemical.registers.item.Elements;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -8,7 +10,39 @@ import java.util.List;
  */
 public class ItemModel {
     public static void registers() {
-//        addItemModels("asa", "item/element/ac");
+        // 元素
+        for (String element : Elements.ELEMENTS_ITEM_ARR) {
+            addItemModels(element, "item/element/" + element);
+        }
+        // 材料
+        // 草纤维
+        addItemModels("grass_fiber", "item/materials/grass_fiber");
+        // 草绳
+        addItemModels("grass_string", "item/materials/grass_string");
+        // 铜板
+        addItemModels("copper_plate", "item/materials/alone/copper/plate");
+        // 铜线
+        addItemModels("copper_wire", "item/materials/alone/copper/wire");
+        // 简易铜线圈
+        addItemModels("easy_copper_coil", "item/materials/alone/copper/easy_copper_coil");
+        // 石墨
+        addItemModels("graphite", "item/materials/graphite");
+
+        // 工具
+        // 燧石剑
+        addItemModels("flint_sword", "item/tool/flint/sword");
+        // 燧石镐
+        addItemModels("flint_pickaxe", "item/tool/flint/pickaxe");
+        // 燧石斧
+        addItemModels("flint_axe", "item/tool/flint/axe");
+        // 燧石手斧
+        addItemModels("flint_handaxe", "item/tool/flint/handaxe");
+        // 燧石锄
+        addItemModels("flint_shovel", "item/tool/flint/shovel");
+        // 燧石锄头
+        addItemModels("flint_hoe", "item/tool/flint/hoe");
+        // 石锤
+        addItemModels("stone_hammer", "item/tool/hammer/stone");
     }
 
     public static final List<List<String>> LIST = new ArrayList<>();
