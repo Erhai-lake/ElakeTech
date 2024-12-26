219e4d9048f3b32113cba4a86abdeee2afd4767e
Qi-Month <dy757410@163.com>
2024-12-27 01:09:58 +0800

    bug fixing

--- src/main/java/top/elake/elaketech/datagen/resources/data/tags/ModItemTagsGen.java
+++ src/main/java/top/elake/elaketech/datagen/resources/data/tags/ModItemTagsGen.java
@@ -1,11 +1,15 @@
 package top.elake.elaketech.datagen.resources.data.tags;
 
+import com.jcraft.jorbis.Block;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
 import net.minecraft.data.tags.ItemTagsProvider;
+import net.minecraft.data.tags.TagsProvider;
 import net.minecraft.world.item.Items;
-import net.minecraft.world.level.block.Block;
+import net.neoforged.neoforge.common.data.ExistingFileHelper;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import top.elake.elaketech.ElakeTech;
 import top.elake.elaketech.tag.ModItemTags;
 
 import java.util.concurrent.CompletableFuture;
@@ -14,8 +18,8 @@ import java.util.concurrent.CompletableFuture;
  * @author Qi-Month
  */
 public class ModItemTagsGen extends ItemTagsProvider {
-    public ModItemTagsGen(PackOutput output, CompletableFuture<HolderLookup.Provider> lookupProvider, CompletableFuture<TagLookup<Block>> blockTags) {
-        super(output, lookupProvider, blockTags);
+    public ModItemTagsGen(PackOutput output, CompletableFuture<HolderLookup.Provider> lookupProvider, CompletableFuture<TagsProvider.TagLookup<Block>> blockTags, @Nullable ExistingFileHelper existingFileHelper) {
+        super(output, lookupProvider, blockTags, ElakeTech.MODID, existingFileHelper);
     }
 
     @Override
--- src/main/java/top/elake/elaketech/tag/ModBlockTags.java
+++ src/main/java/top/elake/elaketech/tag/ModBlockTags.java
@@ -1,7 +1,25 @@
 package top.elake.elaketech.tag;
 
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.ItemTags;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.item.Item;
+
 /**
  * @author Qi-Month
  */
 public class ModBlockTags {
+    /**
+     * 获取neoforge的tag
+     *
+     * @param tag 标签名
+     */
+    private static TagKey<Item> getTag(String tag) {
+        return ItemTags.create(ResourceLocation.fromNamespaceAndPath("c", tag));
+    }
+
+    public static class Items {
+        public static final TagKey<Item> FLINT = getTag("flint");
+        public static final TagKey<Item> BRONZE = getTag("ingots/bronze");
+    }
 }
