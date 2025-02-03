8e2ad741a999723757097de1a5777d73da6ca0bc
Qi-Month <dy757410@163.com>
2025-02-03 12:46:20 +0800

    有铸币忘记导包了

--- src/main/java/top/elake/elaketech/event/RightClickGetGrassFiber.java
+++ src/main/java/top/elake/elaketech/event/RightClickGetGrassFiber.java
@@ -2,6 +2,7 @@ package top.elake.elaketech.event;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.entity.item.ItemEntity;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Blocks;
