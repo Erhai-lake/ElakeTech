0c7d18ab13e092222f79403094f27ec8560c0ba9
xiaoliziawa <3682822069@qq.com>
2025-02-28 13:03:40 +0800

    T1 T2 T3 MetalDetector

--- src/main/java/top/elake/elaketech/register/item/custom/MetalDetectorT3.java
+++ src/main/java/top/elake/elaketech/register/item/custom/MetalDetectorT3.java
@@ -1,5 +1,6 @@
 package top.elake.elaketech.register.item.custom;
 
+import net.minecraft.client.resources.language.I18n;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.network.chat.Component;
@@ -11,17 +12,18 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 import net.neoforged.neoforge.common.Tags;
 import org.jetbrains.annotations.NotNull;
 import top.elake.elaketech.ElakeTech;
 
 /**
- * 一级矿物探测器 - 只显示粒子效果
+ * 三级矿物探测器 - 显示矿物名称和坐标
  * @author Qi-Month
  */
-public class MetalDetector extends Item {
-    public MetalDetector(Properties properties) {
+public class MetalDetectorT3 extends Item {
+    public MetalDetectorT3(Properties properties) {
         super(properties);
     }
 
@@ -37,9 +39,12 @@ public class MetalDetector extends Item {
             
             for (int i = 0; i <= positionClicked.getY() + 64; i++) {
                 BlockState state = context.getLevel().getBlockState(positionClicked.below(i));
+                Block block = state.getBlock();
+                BlockPos foundPos = positionClicked.below(i);
 
                 if (isValuableBlock(state)) {
                     foundBlock = true;
+                    outputValuableCoordinates(foundPos, player, block);
                     spawnFoundParticles(context, player);
                     break;
                 }
@@ -60,10 +65,15 @@ public class MetalDetector extends Item {
         return InteractionResult.SUCCESS;
     }
 
+    private void outputValuableCoordinates(BlockPos blockPos, Player player, Block block) {
+        player.sendSystemMessage(Component.literal(String.format("X: %d, Y: %d, Z: %d - %s",
+                blockPos.getX(), blockPos.getY(), blockPos.getZ(), I18n.get(block.getDescriptionId()))));
+    }
+
     private void spawnFoundParticles(UseOnContext context, Player player) {
         BlockPos playerPos = player.blockPosition();
         // 玩家腰部高度约为1.0格
-        for(int j = 0; j < 5; j++) {
+        for(int j = 0; j < 12; j++) {
             double offsetX = context.getLevel().getRandom().nextDouble() * 0.5D - 0.25D;
             double offsetZ = context.getLevel().getRandom().nextDouble() * 0.5D - 0.25D;
             double offsetY = context.getLevel().getRandom().nextDouble() * 0.2D - 0.1D;
@@ -79,10 +89,7 @@ public class MetalDetector extends Item {
         }
     }
 
-    /**
-     * @param state 方块的状态
-     */
     private boolean isValuableBlock(BlockState state) {
         return state.is(Tags.Blocks.ORES);
     }
-}
+} 
